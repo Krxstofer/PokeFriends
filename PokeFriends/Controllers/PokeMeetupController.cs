5d064cc97f6ad2b02776be8ea4a0da12d62e6099
Kristofer Norman <kjn2418@email.vccs.edu>
2024-04-21 22:38:10 -0400

    added controllers, also added the index views for our pages.

---
+++ PokeFriends/Controllers/PokeMeetupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PokeFriends.Controllers
+{
+    public class PokeMeetupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
